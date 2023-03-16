007af70274a362714a9145d709bf94dd437376a6
Raphael Heide <64502905+raphaelheide@users.noreply.github.com>
2023-03-16 16:45:26 -0300

    Add files via upload

---
+++ includes/index.php
@@ -0,0 +1,24 @@
+<?php 
+$recurring1 = array('No', 'Yes',);
+$recurring2 = array('no_recurring=1', 'recurring=1',);
+$currency1 = array ('Australian Dollars', 'Brazilian Real', 'Canadian Dollars', 'Czech Koruna', 'Danish Krone', 'Euro', 'Hong Kong Dollar', 'Hungarian Forint', 'Israeli New Shekel', 'Yen', 'Malaysian Ringgit', 'Mexican Peso', 'Norwegian Krone', 'New Zealand Dollar', 'Philippine Peso', 'Polish Zloty', 'Pounds Sterling', 'South Africa Rand', 'Russian Ruble', 'Singapore Dollar', 'Swedish Krona', 'Swiss Franc', 'Taiwan New Dollar', 'Thai Baht', 'Turkish Lira', 'U.S. Dollars');
+$currency2 = array ('AUD', 'BRL', 'CAD', 'CZK', 'DKK', 'EUR', 'HKD', 'HUF', 'ILS', 'JPY', 'MYR', 'MXN', 'NOK', 'NZD', 'PHP', 'PLN', 'GBP', 'ZAF', 'RUB', 'SGD', 'SEK', 'CHF', 'TWD', 'THB', 'TRY', 'USD');
+$color1 = array('Red', 'Yellow', 'Blue', 'Green', 'Purple', 'Orange', 'Black', 'Gray', 'Default site color');
+$color2 = array('box-shadow:inset 0px 39px 0px -24px #e67a73; background-color:#e4685d; border-radius:8px; border:1px solid #ffffff; display:inline-block; cursor:pointer; color:#ffffff; font-family:Arial; font-size:15px; padding:9px 27px; text-decoration:none; text-shadow:0px 1px 0px #b23e35;', 
+'box-shadow:inset 0px 39px 0px -24px #ecf06e; background-color:#ebeb3d; border-radius:8px; border:1px solid #ffffff; display:inline-block; cursor:pointer; color:#331f1f; font-family:Arial; font-size:15px; padding:9px 27px; text-decoration:none; text-shadow:0px 1px 0px #9db034;',
+'box-shadow:inset 0px 39px 0px -24px #7387e6; background-color:#4563cb; border-radius:8px; border:1px solid #ffffff; display:inline-block; cursor:pointer; color:#ffffff; font-family:Arial; font-size:15px; padding:9px 27px; text-decoration:none; text-shadow:0px 1px 0px #353db2;',
+'box-shadow:inset 0px 39px 0px -24px #44b162; background-color:#289c3e; border-radius:8px; border:1px solid #ffffff; display:inline-block; cursor:pointer; color:#ffffff; font-family:Arial; font-size:15px; padding:9px 27px; text-decoration:none; text-shadow:0px 1px 0px #35b261;',
+'box-shadow:inset 0px 39px 0px -24px #742f9c; background-color:#8d38b9; border-radius:8px; border:1px solid #ffffff; display:inline-block; cursor:pointer; color:#ffffff; font-family:Arial; font-size:15px; padding:9px 27px; text-decoration:none; text-shadow:0px 1px 0px #7835b2;',
+'box-shadow:inset 0px 39px 0px -24px #d24a01; background-color:#dc6600; border-radius:8px; border:1px solid #ffffff; display:inline-block; cursor:pointer; color:#ffffff; font-family:Arial; font-size:15px; padding:9px 27px; text-decoration:none; text-shadow:0px 1px 0px #b28135;',
+'box-shadow:inset 0px 39px 0px -24px #000000; background-color:#1b1b1b; border-radius:8px; border:1px solid #ffffff; display:inline-block; cursor:pointer; color:#ffffff; font-family:Arial; font-size:15px; padding:9px 27px; text-decoration:none; text-shadow:0px 1px 0px #20201f;',
+'box-shadow:inset 0px 39px 0px -24px #5f5d5d; background-color:#5f5d5a; border-radius:8px; border:1px solid #ffffff; display:inline-block; cursor:pointer; color:#ffffff; font-family:Arial; font-size:15px; padding:9px 27px; text-decoration:none; text-shadow:0px 1px 0px #565653;',
+'" class="button primary',
+);
+
+
+echo '<a href="https://www.paypal.com/donate/?business='
+. get_option('donateme_api') . '&'
+. str_replace($recurring1, $recurring2, get_option('donateme_recurring')) . '&currency_code='
+. str_replace($currency1, $currency2, get_option('donateme_currency')) . '" style="' . str_replace($color1, $color2, get_option('donateme_color')) . ' " >' . get_option('donateme_text') . '</a>'; 
+?>
+
--- readme.txt
+++ readme.txt
@@ -1,80 +1,31 @@
-=== PayPal Donation Plus ===
+=== Donate Me ===
 Contributors: Raphael Heide
-Donate link: https://raphaelheide.com/paypaldonationplus
-Tags: paypal, payment, donation, donations, shortcode, donate, button, paypal donation, add donation, recurrent donation
+Donate link: https://raphaelheide.com
+Tags: donate me, paypal, payment, donation, donations, shortcode, donate, button, paypal donation, add donation, recurrent donation
 Requires at least: 5.5
 Tested up to: 6.1
-Stable tag: 1.0
+Requires PHP: 5.2
+Stable tag: 1.1
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
-Easy, simple setup to add a PayPal Donation Plus button as shortcode.
+Easy. Simple setup to add Donate Me button as shortcode.
 
 == Description ==
-
-Adds a PayPal Donation Plus shortcode in any place of your website. In the settings, add your PayPal email or your ID. In the extra fields, you can define language, return page, button type and size. This Plugin automatically accepts recurring donations.
-
-
-= Shortcode =
-
-Insert the button in your pages or posts with this shortcode
-
-`[paypal-donation-plus]`
-
-= Translations =
-
-The following language translations are already available in the plugin:
-
-* Albanian (sq_AL)
-* Danish (da_DK)
-* Dutch (nl_NL)
-* French (fr_FR)
-* German (de_DE)
-* Hebrew (he_IL)
-* Italian (it_IT)
-* Lithuanian (lt_LT)
-* Malay – Bahasa Melayu (ms_MY)
-* Norwegian bokmål
-* Potuguese (pt_BR)
-* Romanian (ro_RO)
-* Russian (ru_RU)
-* Spanish (es_ES)
-* Swedish (sv_SE)
-* Turkish (tr_TR)
-
-= Related Links =
-
-* [Documentation](https://raphaelheide.com/paypaldonationplus
-  "Instructions")
-
+Adds PayPal donation with Donate Me shortcode in any place of your website. In the settings, add your PayPal email or your ID. 
 
 == Installation ==
-
-= Install =
-
-1. Upload the 'paypal-donations-plus' folder  to the '/wp-content/plugins/' directory.
-2. Activate the plugin through the 'Plugins' menu in WordPress.
-3. Go to Settings -> PayPal Donation Plus and add your informations.
-
-= Uninstall =
-
-1. Deactivate PayPal Donation Plus in the 'Plugins' menu in Wordpress.
-2. After Deactivation a 'Delete' link appears below the plugin name, follow the link and confirm with 'Yes, Delete these files'.
-3. This will delete all the plugin files from the server as well as erasing all options the plugin has stored in the database.
-
+1. Upload the \'donate-me\' folder  to the \'/wp-content/plugins/\' directory.
+2. Activate the plugin through the \'Plugins\' menu in WordPress.
+3. Go to Settings -> Donate Me and add your information.
+4. Insert the button in your pages or posts with this shortcode
+`[donateme]`
 
 == Frequently Asked Questions ==
-
-= Where can I get support? =
-
-
-== Screenshots ==
-
-1. Admin page where you setup the plugin
-1. Sidebar Widget Admin Options
+Documentation:
+https://raphaelheide.com/donateme
 
 
 == Changelog ==
-
-= version 1.1 =
+Version 1.1
 * Initial Release
